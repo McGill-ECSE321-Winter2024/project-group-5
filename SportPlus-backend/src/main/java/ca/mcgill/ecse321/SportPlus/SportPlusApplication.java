35fd0e8bdf17a62d7b4d509504eba90a6ce972f1
Sophia Carbone <144263016+ezlilcoconut@users.noreply.github.com>
2024-02-13 19:25:59 -0500

    Spring initializer setup

---
+++ SportPlus-backend/src/main/java/ca/mcgill/ecse321/SportPlus/SportPlusApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.SportPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportPlusApplication.class, args);
+	}
+
+}
