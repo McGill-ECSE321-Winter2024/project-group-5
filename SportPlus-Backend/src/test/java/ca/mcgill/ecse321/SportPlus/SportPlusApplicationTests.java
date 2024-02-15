5f4ed7217f0214abd4082b11143cce0d1f7f8583
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2024-02-15 16:24:31 -0500

    added springboot intializer files

---
+++ SportPlus-Backend/src/test/java/ca/mcgill/ecse321/SportPlus/SportPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.SportPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
