ada6a96c4ac11b9f08df4949991b390df2242280
Sophia Carbone <sophia.carbone@mail.mcgill.ca>
2024-02-15 19:35:10 -0500

    starting CRUD, figuring it out as i go

--- SportPlus-backend/src/main/java/ca/mcgill/ecse321/SportPlus/SportPlusApplication.java
+++ SportPlus-backend/src/main/java/ca/mcgill/ecse321/SportPlus/SportPlusApplication.java
@@ -2,13 +2,20 @@ package ca.mcgill.ecse321.SportPlus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SportPlusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportPlusApplication.class, args);
 		// testing vscode
+	}
+	@RequestMapping("/")
+	public String greeting() {
+		return "Hello world!";
 	}
 
 }
