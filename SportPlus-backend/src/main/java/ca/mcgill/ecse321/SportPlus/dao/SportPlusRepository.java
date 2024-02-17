1c9338872c145ebe47688f5f26c823b10adf3b90
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2024-02-17 00:32:32 -0500

    SportPlusRepository for ability to save into databse and create objects with SportPlus as an attribute

---
+++ SportPlus-backend/src/main/java/ca/mcgill/ecse321/SportPlus/dao/SportPlusRepository.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse321.SportPlus.dao;
+
+import ca.mcgill.ecse321.SportPlus.model.SportPlus;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SportPlusRepository extends CrudRepository<SportPlus, String> {
+
+}
