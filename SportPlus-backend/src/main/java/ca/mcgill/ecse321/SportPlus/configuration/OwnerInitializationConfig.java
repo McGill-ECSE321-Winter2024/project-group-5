6d82f4fba40b25462eff15486c228f4679e5f6ad
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2024-03-13 16:33:31 -0400

    owner in configuration will not have a preset firstName, lastName, or password, but the email will be preset

--- SportPlus-backend/src/main/java/ca/mcgill/ecse321/SportPlus/configuration/OwnerInitializationConfig.java
+++ SportPlus-backend/src/main/java/ca/mcgill/ecse321/SportPlus/configuration/OwnerInitializationConfig.java
@@ -10,14 +10,10 @@ public class OwnerInitializationConfig {
     @Autowired
     private OwnerService ownerService;
 
-    String firstName = "OwnerFirstName";
-    String lastName = "OwnerLastName";
-    String password = "OwnerPassword";
-
     @jakarta.annotation.PostConstruct
     public void initializeOwner() {
         if (ownerService.getOwner() == null) {
-            ownerService.createOwner(firstName, password, lastName);
+            ownerService.createOwner();
         }
     }
     
