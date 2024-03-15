d33324c2efb1b0919407a9577af0391452c9b1e6
Dmytro <dmytromartyniuk2003@gmail.com>
2024-03-14 22:06:02 -0400

    Added an empty test to specifcclass tests

--- SportPlus-backend/src/test/java/ca/mcgill/ecse321/SportPlus/Service/TestSpecificClassService.java
+++ SportPlus-backend/src/test/java/ca/mcgill/ecse321/SportPlus/Service/TestSpecificClassService.java
@@ -82,4 +82,8 @@ public class TestSpecificClassService {
         verify(specificClassRepository).save(any(SpecificClass.class));
     }
 
+    @Test
+    public void createRecurringSpecificClassesTest() {
+    }
+
 }
