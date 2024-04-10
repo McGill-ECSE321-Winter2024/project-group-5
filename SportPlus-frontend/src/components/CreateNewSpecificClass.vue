e856ed7f228ccfd74eeac83b4d4ae0180cbf763d
SirineTarhouni <sirine.tarhouni@mail.mcgill.ca>
2024-04-10 09:24:56 -0400

    worked on specific class

--- SportPlus-frontend/src/components/CreateNewSpecificClass.vue
+++ SportPlus-frontend/src/components/CreateNewSpecificClass.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="events">
-      <div>
-          <b-form-select v-model="selectedClassType" :options="options" placeholder="Select ClassType"></b-form-select>
+  <div>
+      <div id="events">
+          <b-form-select v-model="selectedClassType" :options="classTypeOptions" placeholder="Select a class type"></b-form-select>
+          <b-form-select v-model="selectedInstructor" :options="instructorOptions" placeholder="Select an instructor"></b-form-select>
           <input type="date" placeholder="Date" v-model="newEventDate" />
-          <b-form-select v-model="selectedTime" :options="options"></b-form-select>
-          <b-form-select v-model="selectedInstructor" :options="options"></b-form-select>
-          <input type="time" placeholder="Start Time" v-model="newEventStartTime" />
-          <input type="time" placeholder="End Time" v-model="newEventEndTime" />
+          <b-form-select v-model="selectedTime" :options="timeOptions" placeholder="Select a time slot"></b-form-select>
           <button @click="createEvent()" v-bind:disabled="isCreateBtnDisabled">Create Event</button>
           <button class="danger-btn" @click="clearInputs()">Clear</button>
       </div>
@@ -32,9 +30,14 @@
 import axios from "axios";
 import config from "../../config";
 
+const frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+const backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+
 const client = axios.create({
   // IMPORTANT: baseURL, not baseUrl
-  baseURL: config.dev.backendBaseUrl
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
 });
 
 export default {
@@ -47,13 +50,21 @@ export default {
           newEventStartTime: null,
           newEventEndTime: null,
           newEventRegLimit: null,
-          newEventLocation: null
+          newEventLocation: null,
+          selectedClassType: null,
+          selectedInstructor: null,
+          classTypeOptions: [ { text: 'Select a class type', value: null },{ text: 'Option 1', value: 'option1' },{ text: 'Option 2', value: 'option2' }],
+          instructorOptions: [{ text: 'Select an instructor', value: null }],
+          selectedTime: null,
+          timeOptions: [{ text: 'Select a time slot', value: null }]
+
       };
   },
   async created() {
       try {
-          const response = await client.get("/events");
-          this.events = response.data.events
+          this.fetchClassTypes();
+          this.fetchInstructors();
+          this.generateTimeOptions();
       }
       catch (e) {
           // TODO: show the user a warning
@@ -61,6 +72,53 @@ export default {
       }
   },
   methods: {
+    
+    async fetchClassTypes(){
+        try{
+            let endpointPath = '';
+            endpointPath = `/classType/all`;
+            const fullUrl = `http://${config.dev.backendHost}:${config.dev.backendPort}${endpointPath}`;
+            const userResponse = await axios.get(fullUrl);
+
+            this.classTypeOptions = userResponse.data;
+
+        }catch(e){
+            // TODO: show the user a warning
+            console.log(e);
+        }
+    },
+
+    async fetchInstructors(){
+        try{
+            let endpointPath = '';
+            endpointPath = `/instructors/all`;
+            const fullUrl = `http://${config.dev.backendHost}:${config.dev.backendPort}${endpointPath}`;
+            const userResponse = await axios.get(fullUrl);
+
+            this.instructorOptions = userResponse.data;
+
+        }catch(e){
+            // TODO: show the user a warning
+            console.log(e);
+        }
+    },
+
+    generateTimeOptions() {
+      const startTime = 8; // Starting hour (8 am)
+      const endTime = 18; // Ending hour (6 pm)
+
+      for (let i = startTime; i < endTime; i ++) {
+        const startHour = i.toString().padStart(2, '0'); // Format start hour
+        const endHour = (i + 1).toString().padStart(2, '0'); // Format end hour
+        const slotLabel = `${startHour}:00 - ${endHour}:00`; // Combine start and end times
+        this.timeOptions.push({ text: slotLabel, value: startHour });
+      }
+    },
+
+    formatTime(time) {
+      return `${time.toString().padStart(2, '0')}:00`; // Format time as HH:00
+    },
+
       async createEvent() {
           const newEvent = {
               type: "IN_PERSON",
