70b54dbadd8c940726eee4c3b9fd5088943aba90
SirineTarhouni <sirine.tarhouni@mail.mcgill.ca>
2024-04-10 21:30:15 -0400

    create specific class type for singular class working

--- SportPlus-frontend/src/components/CreateNewSpecificClass.vue
+++ SportPlus-frontend/src/components/CreateNewSpecificClass.vue
@@ -1,128 +1,277 @@
 <template>
-  <div id="events">
-      <div>
-          <b-form-select v-model="selectedClassType" :options="options" placeholder="Select ClassType"></b-form-select>
-          <input type="date" placeholder="Date" v-model="newEventDate" />
-          <b-form-select v-model="selectedTime" :options="options"></b-form-select>
-          <b-form-select v-model="selectedInstructor" :options="options"></b-form-select>
-          <input type="time" placeholder="Start Time" v-model="newEventStartTime" />
-          <input type="time" placeholder="End Time" v-model="newEventEndTime" />
-          <button @click="createEvent()" v-bind:disabled="isCreateBtnDisabled">Create Event</button>
-          <button class="danger-btn" @click="clearInputs()">Clear</button>
-      </div>
-      <h2>New Specific Class</h2>
-      <table>
-          <tbody id="events-tbody">
-              <tr>
-                  <th>Name</th>
-                  <th>Date</th>
-                  <th>Limit</th>
-              </tr>
-              <tr v-for="e in events">
-                  <td>{{ e.name }}</td>
-                  <td>{{ e.date }}</td>
-                  <td>{{ e.registrationLimit }}</td>
-              </tr>
-          </tbody>
-      </table>
-  </div>
-</template>
-
-<script>
-import axios from "axios";
-import config from "../../config";
-
-const client = axios.create({
-  // IMPORTANT: baseURL, not baseUrl
-  baseURL: config.dev.backendBaseUrl
-});
-
-export default {
-  name: "CreateNewSpecificClass",
-  data() {
-      return {
-          events: [],
-          newEventName: null,
-          newEventDate: null,
-          newEventStartTime: null,
-          newEventEndTime: null,
-          newEventRegLimit: null,
-          newEventLocation: null
-      };
-  },
-  async created() {
-      try {
-          const response = await client.get("/events");
-          this.events = response.data.events
-      }
-      catch (e) {
-          // TODO: show the user a warning
-          console.log(e);
-      }
-  },
-  methods: {
-      async createEvent() {
-          const newEvent = {
-              type: "IN_PERSON",
-              name: this.newEventName,
-              date: this.newEventDate,
-              startTime: this.newEventStartTime,
-              endTime: this.newEventEndTime,
-              registrationLimit: this.newEventRegLimit,
-              location: this.newEventLocation
-          };
-          try {
-              const response = await client.post("/events", newEvent);
-              this.events.push(response.data);
-              this.clearInputs();
+    <div>
+        <div id="newSpecificClasses">
+            <b-form-select v-model="selectedClassType" :options="classTypeOptions" placeholder="Select a class type"></b-form-select>
+            <b-form-select v-model="selectedInstructor" :options="instructorOptions" placeholder="Select an instructor"></b-form-select>
+            <input type="date" placeholder="Date" v-model="newSpecificClassDate" />
+            <b-form-select v-model="selectedTime" :options="timeOptions" placeholder="Select a time slot"></b-form-select>
+            <button @click="createSpecificClass()" v-bind:disabled="isCreateBtnDisabled">Create Event</button>
+            <button class="danger-btn" @click="clearInputs()">Clear</button>
+        </div>
+        <h2>New Specific Class</h2>
+        <table>
+            <tbody id="newSpecificClasses-tbody">
+                <tr>
+                    <th>Name</th>
+                    <th>Date</th>
+                </tr>
+                <tr v-for="s in newSpecificClasses">
+                    <td>{{ s.name }}</td>
+                    <td>{{ s.date }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  import config from "../../config";
+  import { globalState } from "@/global.js";
+  
+  const frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+  const backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+  
+  
+  const CLIENT = axios.create({
+    // IMPORTANT: baseURL, not baseUrl
+    baseURL: backendUrl,
+    headers: { 'Access-Control-Allow-Origin': frontendUrl }
+  });
+  //add recurring class 
+  //add filter approved class type
+  
+  export default {
+    name: "CreateNewSpecificClass",
+    data() {
+        return {
+            newSpecificClasses: [],
+            newSpecificClassDate: null,
+            newSpecificClassStartTime: null,
+            newSpecificClassEndTime: null,
+            selectedClassType: null,
+            selectedInstructor: null,
+            selectedTime: null,
+            classTypeOptions: [ { text: 'Select a class type', value: null }],
+            instructorOptions: [{ text: 'Select an instructor', value: null }],
+            timeOptions: [{ text: 'Select a time slot', value: null }]
+            
+        };
+    },
+   mounted() {
+        try {
+            this.fetchClassTypes();
+            this.fetchInstructors();
+            this.generateTimeOptions();
+        }
+        catch (e) {
+            // TODO: show the user a warning
+            console.log(e);
+        }
+    },
+    methods: {
+  
+     
+      async fetchClassTypes(){
+           // Make an HTTP GET request to fetch classTypes
+           CLIENT.get('/classType/all')
+                      .then(response => {
+                          const resp = response.data.classTypes;
+                          console.log("responseClassTypes", resp);
+                          const approvedClassTypes = resp.filter(type => type.approved);
+                          console.log("approvedTypes", approvedClassTypes);
+                          const classTypesData = approvedClassTypes.map(classType =>({
+                              text: `${classType.name}`,
+                              value: classType.typeId,
+                          }));
+                          
+                      // Assign the retrieved classTypes to types array
+                      classTypesData.forEach(classType => {
+                          this.classTypeOptions.push(classType);
+                      });
+  
+                      })
+                      .catch(error => {
+                      console.error('Error fetching classTypes:', error);
+                      });
+      },
+  
+      async fetchInstructors(){
+          // Make an HTTP GET request to fetch instructors
+          CLIENT.get('/instructors/all')
+                      .then(response => {
+                          const resp = response.data.instructors;
+                          console.log("responseInstructors", resp);
+                          
+                          const instructorsData = response.data.instructors.map(instructor => ({
+                          text: `${instructor.lastName}, ${instructor.firstName}`, // Instructor name as the display text
+                          value: instructor.accountId // Instructor ID as the value
+                          }));
+                      // Assign the retrieved instructors to the instructors array
+                      instructorsData.forEach(instructor => {
+                          this.instructorOptions.push(instructor);
+                      });
+  
+                      })
+                      .catch(error => {
+                      console.error('Error fetching instructors:', error);
+                      });
+      },
+  
+      async fetchClassType(){
+          const classTypeName = this.getClassTypeName(this.selectedClassType);
+          CLIENT.get(`/classType/get/`+classTypeName)
+          .then(response => {
+                          return response.data.classType.typeId;
+  
+                      })
+          .catch(error => {
+                      console.error('Error fetching selected class Type:', error);
+                      });
+      },
+  
+     
+  
+      generateTimeOptions() {
+        const startTime = 8; // Starting hour (8 am)
+        const endTime = 18; // Ending hour (6 pm)
+  
+        for (let i = startTime; i < endTime; i ++) {
+          const startHour = i.toString().padStart(2, '0'); // Format start hour
+          const endHour = (i + 1).toString().padStart(2, '0'); // Format end hour
+          const slotLabel = `${startHour}:00 - ${endHour}:00`; // Combine start and end times
+          this.timeOptions.push({ text: slotLabel, value: startHour });
+        }
+      },
+  
+      formatTime(time) {
+        return `${time.toString().padStart(2, '0')}:00`; // Format time as HH:00
+      },
+  
+      getClassTypeName(selectedValue) {
+          const selectedOption = this.classTypeOptions.find(option => option.value === selectedValue);
+          return selectedOption ? selectedOption.text : null;
+      },
+  
+      getStartTime(selectedValue) {
+          const selectedOption = this.timeOptions.find(option => option.value === selectedValue);
+          if (selectedOption) {
+              const timeString = selectedOption.text.split(':')[0]; // Extract the hour part
+              return parseInt(timeString); // Parse the hour as an integer
           }
-          catch (e) {
-              // TODO: show the user a warning
-              console.log(e);
-          }
+          return null;
       },
       clearInputs() {
-          this.newEventName = null;
-          this.newEventDate = null;
-          this.newEventStartTime = null;
-          this.newEventEndTime = null;
+            this.selectedTime = null;
+            this.newSpecificClassDate = null;
+            this.selectedClassType = null;
+            this.selectedInstructor = null;
+    
+        },
   
-      }
-  },
-  computed: {
-      isCreateBtnDisabled() {
-          return (
-              !this.newEventName
-              || !this.newEventDate
-              || !this.newEventStartTime
-              || !this.newEventEndTime
-              || !this.newEventLocation
-          );
-      }
+        async createSpecificClass() {
+          const startTime = this.getStartTime(this.selectedTime); 
+          const endTime = startTime + 1;
+          console.log("date", new Date(this.newSpecificClassDate));
+          console.log("start time", startTime )
+          console.log("end time", new Date(this.newSpecificClassEndTime))
+          console.log("instroctorId", this.selectedInstructor)
+          console.log("classTypeId", this.selectedClassType)
+          
+  
+              const date = new Date(this.newSpecificClassDate);
+              const year = date.getFullYear();
+              const month = date.getMonth()+1;
+              const day = date.getDate()+1;
+  
+              let javaDate = null;
+              if(month < 10 && day < 10){
+                  javaDate = year + '-0' + month +'-0'+ day;
+              }
+              else if(month < 10 && day >= 10){
+                  javaDate = year + '-0' + month +'-'+ day;
+              }
+              else if(month >= 10 && day < 10){
+                  javaDate = year +'-'+ month +'-0'+day ;
+              }else{
+                  javaDate = year + '-'+ month+'-'+day;
+              }
+              console.log("date java", javaDate);
+  
+              let javaStartTime = 0;
+              let javaEndTime = 0;
+  
+              if(startTime < 10){
+                  javaStartTime = '0'+startTime+':00:00';
+              }else{
+                  javaStartTime = startTime+':00:00';
+              }
+  
+              if(endTime < 10){
+                  javaEndTime = '0'+endTime+':00:00';
+              }else{
+                  javaEndTime = endTime+':00:00';
+              }
+  
+              console.log("start time java : ", javaStartTime)
+              console.log("end time java : ", javaEndTime)
+  
+            const newSpecificClass = {
+              date: javaDate,
+              startTime: javaStartTime,
+              endTime: javaEndTime,
+              instructorId : this.selectedInstructor ,
+              classTypeId :  this.selectedClassType,
+            };
+            console.log("specific class", newSpecificClass)
+            try {
+                const response = await CLIENT.post('/specificClass/create', newSpecificClass);
+                this.newSpecificClasses.push(response.data);
+                this.clearInputs();
+            }
+            catch (e) {
+                // TODO: show the user a warning
+                console.log(e);
+            }
+        }
+  
+        
+    },
+    computed: {
+        isCreateBtnDisabled() {
+            return (
+                 !this.newSpecificClassDate
+                || !this.selectedTime
+                || !this.selectedClassType
+                || !this.selectedInstructor
+            );
+        }
+  
+    }
+  };
+  </script>
+  
+  <style>
+  #newSpecificClasses {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
   }
-};
-</script>
-
-<style>
-#events {
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-}
-
-h2 {
-  padding-top: 1em;
-  text-decoration: underline;
-}
-
-td,
-th {
-  padding: 0.5em;
-  border: 1px solid black;
-}
-
-.danger-btn {
-  border: 1px solid red;
-  color: red;
-}
-</style>
+  
+  h2 {
+    padding-top: 1em;
+    text-decoration: underline;
+  }
+  
+  td,
+  th {
+    padding: 0.5em;
+    border: 1px solid black;
+  }
+  
+  .danger-btn {
+    border: 1px solid red;
+    color: red;
+  }
+  </style>
+  
